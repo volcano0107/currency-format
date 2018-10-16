57973e76f286e0edf6783d01e71e85e3bc91632f
王灿 <wangcan@youxin.com>
2018-10-16 16:07:33 +0800

    add coveralls istanbul

--- test/test.js
+++ test/test.js
@@ -4,6 +4,18 @@ var expect = require('chai').expect;
 var currencyFormat = require('../index');
 
 describe('#currencyFormat', function() {
+    it('值为null', function() {
+        var result = currencyFormat(null);
+        expect(result).to.equal('');
+    });
+    it('负数', function() {
+        var result = currencyFormat(-2);
+        expect(result).to.equal('-$2.00');
+    });
+    it('值为0', function() {
+        var result = currencyFormat(0);
+        expect(result).to.equal('$0.00');
+    });
     it('一位数', function() {
         var result = currencyFormat(1);
         expect(result).to.equal('$1.00');
